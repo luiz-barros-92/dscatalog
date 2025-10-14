5f5fc38985e5ba023e02820049e3698d35b82420
Luiz Barros <lck.barros@gmail.com>
2025-10-13 21:55:15 -0300

    feat: create Category entity
    chore: add ServerON! message to console in first boot

--- backend/src/main/java/com/luizbarros/dscatalog/DscatalogApplication.java
+++ backend/src/main/java/com/luizbarros/dscatalog/DscatalogApplication.java
@@ -1,13 +1,18 @@
 package com.luizbarros.dscatalog;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DscatalogApplication {
+public class DscatalogApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DscatalogApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Server ON!");		
+	}
 }
