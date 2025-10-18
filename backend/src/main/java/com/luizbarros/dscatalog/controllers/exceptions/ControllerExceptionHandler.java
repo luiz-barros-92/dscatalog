41d4929e61f08bf60f5077e95c243ba6f3d60c14
Luiz Barros <lck.barros@gmail.com>
2025-10-17 21:07:41 -0300

    feat: implement DatabaseException handler

--- backend/src/main/java/com/luizbarros/dscatalog/controllers/exceptions/ControllerExceptionHandler.java
+++ backend/src/main/java/com/luizbarros/dscatalog/controllers/exceptions/ControllerExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.luizbarros.dscatalog.services.exceptions.DatabaseException;
 import com.luizbarros.dscatalog.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -21,6 +22,18 @@ public class ControllerExceptionHandler {
 			Instant.now(),
 			status.value(),
 			"Entity not found",
+			e.getMessage(),
+			request.getRequestURI());
+		return ResponseEntity.status(status).body(error);		
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(ResourceNotFoundException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError error = new StandardError(
+			Instant.now(),
+			status.value(),
+			"Database exception",
 			e.getMessage(),
 			request.getRequestURI());
 		return ResponseEntity.status(status).body(error);		
