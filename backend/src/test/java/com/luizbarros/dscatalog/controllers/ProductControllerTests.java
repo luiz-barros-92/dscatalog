1fc14da348a77a3612368015f3d134b963fda07a
Luiz Barros <lck.barros@gmail.com>
2025-10-30 19:56:05 -0300

    test: findAll in ProductControllerTests

--- backend/src/test/java/com/luizbarros/dscatalog/controllers/ProductControllerTests.java
+++ backend/src/test/java/com/luizbarros/dscatalog/controllers/ProductControllerTests.java
@@ -2,13 +2,17 @@ package com.luizbarros.dscatalog.controllers;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -36,4 +40,10 @@ public class ProductControllerTests {
 		when(service.findAll(any())).thenReturn(page);
 	}
 	
+	@Test
+	public void findAllShouldReturnPage() throws Exception {		
+		mockMvc.perform(get("/products")
+			.accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk());
+	}	
 }
