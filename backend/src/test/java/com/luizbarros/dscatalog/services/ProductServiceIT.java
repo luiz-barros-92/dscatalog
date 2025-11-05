cfee9f2af70fd17ba42bceec4cbc29f1730d77c2
Luiz Barros <lck.barros@gmail.com>
2025-11-04 22:44:46 -0300

    test: findAll in ProductServiceIT

--- backend/src/test/java/com/luizbarros/dscatalog/services/ProductServiceIT.java
+++ backend/src/test/java/com/luizbarros/dscatalog/services/ProductServiceIT.java
@@ -1,17 +1,25 @@
 package com.luizbarros.dscatalog.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.luizbarros.dscatalog.dto.ProductDTO;
 import com.luizbarros.dscatalog.repositories.ProductRepository;
 import com.luizbarros.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @SpringBootTest
+@Transactional
 public class ProductServiceIT {
 
 	@Autowired
@@ -40,5 +48,35 @@ public class ProductServiceIT {
 	@Test
 	public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
 		assertThrows(ResourceNotFoundException.class, () -> service.findById(nonExistingId));
+	}
+	
+	@Test
+	public void findAllPagedShouldReturnPageWithCorrectPaginationDetails() {
+		PageRequest pageRequest = PageRequest.of(0, 10);
+		Page<ProductDTO> result = service.findAll(pageRequest);
+		assertFalse(result.isEmpty());
+		assertEquals(0, result.getNumber());
+		assertEquals(10, result.getSize());
+		assertEquals(countTotalProducts, result.getTotalElements());
+	}
+	
+	@Test
+	public void findAllPagedShouldReturnEmptyPageWhenPageDoesNotExist() {
+		PageRequest pageRequest = PageRequest.of(50, 10);
+		Page<ProductDTO> result = service.findAll(pageRequest);
+		assertTrue(result.isEmpty());
+	}
+	
+	@Test
+	public void findAllPagedShouldReturnSortedPageWhenSortedByName() {
+		PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("name"));
+		Page<ProductDTO> result = service.findAll(pageRequest);
+		assertFalse(result.isEmpty());
+		assertEquals("Macbook Pro", result.getContent().get(0).name());
+		assertEquals("PC Gamer", result.getContent().get(1).name());
+		assertEquals("PC Gamer Alfa", result.getContent().get(2).name());
+		assertEquals("PC Gamer Boo", result.getContent().get(3).name());
+		assertEquals("PC Gamer Card", result.getContent().get(4).name());
+		assertEquals("PC Gamer Er", result.getContent().get(5).name());
 	}
 }
