815b7f3036a869477f5628f8f08d9e740d7ee178
Luiz Barros <lck.barros@gmail.com>
2025-11-05 22:16:40 -0300

    test: update in ProductControllerIT

--- backend/src/test/java/com/luizbarros/dscatalog/controllers/ProductControllerIT.java
+++ backend/src/test/java/com/luizbarros/dscatalog/controllers/ProductControllerIT.java
@@ -1,6 +1,7 @@
 package com.luizbarros.dscatalog.controllers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -13,6 +14,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.luizbarros.dscatalog.dto.ProductDTO;
+import com.luizbarros.dscatalog.tests.Factory;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
@@ -24,12 +29,21 @@ public class ProductControllerIT {
 	private Long existingId;
 	private Long nonExistingId;
 	private Long countTotalProducts;
+	private ProductDTO productDTO;
+	private String expectedName;
+	private String expectedDescription;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		existingId = 1L;
 		nonExistingId = 1000L;
-		countTotalProducts = 25L;		
+		countTotalProducts = 25L;
+		productDTO = Factory.createProductDTO();
+		expectedName = productDTO.name();
+		expectedDescription = productDTO.description();
 	}
 	
 	@Test
@@ -45,5 +59,28 @@ public class ProductControllerIT {
 		.andExpect(jsonPath("$.content[3].name").value("PC Gamer Boo"))
 		.andExpect(jsonPath("$.content[4].name").value("PC Gamer Card"))
 		.andExpect(jsonPath("$.content[5].name").value("PC Gamer Er"));
+	}
+	
+	@Test
+	public void updateShouldReturnProductDTOWhenIdExists() throws Exception  {
+		String jsonBody = objectMapper.writeValueAsString(productDTO);
+		mockMvc.perform(put("/products/{id}", existingId)
+			.content(jsonBody)
+			.contentType(MediaType.APPLICATION_JSON)
+			.accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.id").value(existingId))
+			.andExpect(jsonPath("$.name").value(expectedName))
+			.andExpect(jsonPath("$.description").value(expectedDescription));
+	}
+	
+	@Test
+	public void updateShouldReturnNotFoundWhenIdDoesNotExist() throws Exception  {
+		String jsonBody = objectMapper.writeValueAsString(productDTO);
+		mockMvc.perform(put("/products/{id}", nonExistingId)
+			.content(jsonBody)
+			.contentType(MediaType.APPLICATION_JSON)
+			.accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isNotFound());
 	}
 }
