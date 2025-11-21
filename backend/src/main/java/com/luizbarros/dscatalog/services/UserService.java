219826ab676c467cf80f58bc2ba3e3cd3b4fac07
Luiz Barros <lck.barros@gmail.com>
2025-11-20 23:22:10 -0300

    feat: Spring Security checklist

--- backend/src/main/java/com/luizbarros/dscatalog/services/UserService.java
+++ backend/src/main/java/com/luizbarros/dscatalog/services/UserService.java
@@ -1,8 +1,13 @@
 package com.luizbarros.dscatalog.services;
 
+import java.util.List;
+
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -14,6 +19,7 @@ import com.luizbarros.dscatalog.dto.UserInsertDTO;
 import com.luizbarros.dscatalog.dto.UserUpdateDTO;
 import com.luizbarros.dscatalog.entities.Role;
 import com.luizbarros.dscatalog.entities.User;
+import com.luizbarros.dscatalog.projections.UserDetailsProjection;
 import com.luizbarros.dscatalog.repositories.RoleRepository;
 import com.luizbarros.dscatalog.repositories.UserRepository;
 import com.luizbarros.dscatalog.services.exceptions.DatabaseException;
@@ -22,7 +28,7 @@ import com.luizbarros.dscatalog.services.exceptions.ResourceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
 
 @Service
-public class UserService {	
+public class UserService implements UserDetailsService{	
 	
 	private final UserRepository userRepository;
 	private final RoleRepository roleRepository;
@@ -92,5 +98,20 @@ public class UserService {
         	Role role = roleRepository.getReferenceById(roleDto.getId());
         	entity.getRoles().add(role);
         }
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		List<UserDetailsProjection> result = userRepository.searchUserAndRolesByEmail(username);
+		if(result.size() == 0) {
+			throw new UsernameNotFoundException("User not found");
+		}
+		User user = new User();
+		user.setEmail(username);
+		user.setPassword(result.get(0).getPassword());
+		for(UserDetailsProjection projection : result) {
+			user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
+		}
+		return user;
 	}
 }
