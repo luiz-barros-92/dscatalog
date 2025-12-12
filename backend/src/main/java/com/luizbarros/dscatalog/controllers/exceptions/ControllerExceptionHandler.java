885f38b1fd32bc432ee034f0103c726bc444f989
Luiz Barros <lck.barros@gmail.com>
2025-12-11 23:19:30 -0300

    feat: email exception handler

--- backend/src/main/java/com/luizbarros/dscatalog/controllers/exceptions/ControllerExceptionHandler.java
+++ backend/src/main/java/com/luizbarros/dscatalog/controllers/exceptions/ControllerExceptionHandler.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.luizbarros.dscatalog.services.exceptions.DatabaseException;
+import com.luizbarros.dscatalog.services.exceptions.EmailException;
 import com.luizbarros.dscatalog.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -54,7 +55,18 @@ public class ControllerExceptionHandler {
 		for (FieldError f : e.getBindingResult().getFieldErrors()) {
 			error.addError(f.getField(), f.getDefaultMessage());
 		}		
-		
+		return ResponseEntity.status(status).body(error);		
+	}
+	
+	@ExceptionHandler(EmailException.class)
+	public ResponseEntity<StandardError> email(EmailException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError error = new StandardError(
+			Instant.now(),
+			status.value(),
+			"Email exception",
+			e.getMessage(),
+			request.getRequestURI());
 		return ResponseEntity.status(status).body(error);		
 	}
 }
